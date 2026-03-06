841988b413beba3f36023cd9de3341d895e37661
Nikki <nikhilpareeshwad@gmail.com>
2026-03-06 20:02:01 +0530

    modify axios.js in frontend

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -3,6 +3,8 @@ import API from "./axios"
 
 export const getTasks = (params) => { return API.get("/tasks", params); }
 export const createTask = (data) => { return API.post("/tasks", data); }
-// export const getTasksById = (id) => { return API.get(`/tasks/:${id}`, id); }
+
+
+
 export const updateTask = (id, data) => { return API.put(`/tasks/${id}`, data); }
 export const deleteTask = (id) => { return API.delete(`/tasks/${id}`); }
